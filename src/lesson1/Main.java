f9361035c11ee0ffb02b82b30e0ac37d0a054c5d
ZiganshinIB <ZiganshinIB@nknh.sibur.ru>
2023-03-25 21:22:43 +0300

    Finish Seminar 1

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -7,9 +7,33 @@ import lesson1.task1.TaskLess_1;
 
 
 import java.time.LocalDateTime;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Задача 1");
+        System.out.println("Введите Число");
+        int number1 = scanner.nextInt();
+        System.out.println(TaskLess_1.arithmeticProgression(number1));
+        System.out.println("Задача 2");
+        System.out.println("Введите Число для фактроила: ");
+        int number2 = scanner.nextInt();
+        System.out.println(TaskLess_1.factorial(number2));
+        System.out.println("Задача 3");
         TaskLess_1.printPrimeNumbers(1000);
+
+        System.out.println("Задача 4");
+        System.out.print("Введите Число 1: ");
+        double a = scanner.nextDouble();
+        System.out.print("Введите Число 2: ");
+        double b = scanner.nextDouble();
+        System.out.print("Введите что будем делать (+, -, *, /): ");
+        char point = scanner.next().charAt(0);
+        System.out.println(TaskLess_1.calc(a, b, point));
+
+        scanner.close();
+
     }
 }
--- src/lesson1/task1/TaskLess_1.java
+++ src/lesson1/task1/TaskLess_1.java
@@ -4,15 +4,15 @@ import java.math.BigInteger;
 
 public class TaskLess_1 {
     public static int arithmeticProgression(int n, int start, int step){
-        return ((2 * start -  step*(n-1))*n)/2;
+        return ((2 * start + step*(n-1))*n)/2;
     }
 
     public static int arithmeticProgression(int n, int start){
-        return ((2 * start - (n-1))*n)/2;
+        return ((2 * start + (n-1))*n)/2;
     }
 
     public static int arithmeticProgression(int n){
-        return ((2 - (n-1))*n)/2;
+        return ((2 + (n-1))*n)/2;
     }
 
     public static int factorial(int n){
@@ -27,5 +27,20 @@ public class TaskLess_1 {
     private static boolean isPrimeNumber(int number){
         for(int i=2; i < number; i++) if ((number%i)==0) return false;
         return true;
+    }
+
+    public static double calc(double a, double b, char point){
+        switch (point) {
+            case  ('+'):
+                return a+b;
+            case ('-'):
+                return a-b;
+            case ('*'):
+                return a*b;
+            case ('/'):
+                return a/b;
+            default:
+                return 0.0d;
+        }
     }
 }
