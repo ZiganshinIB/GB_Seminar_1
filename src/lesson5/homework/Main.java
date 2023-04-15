d53b91359d58bf30af16cec3ce4cb40378e03955
ZiganshinIB <ZiganshinIB@nknh.sibur.ru>
2023-04-15 15:15:26 +0300

    Feat Seminar 5: homework -> romanToArabic

--- src/lesson5/homework/Main.java
+++ src/lesson5/homework/Main.java
@@ -1,8 +1,6 @@
 package lesson5.homework;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
@@ -18,6 +16,32 @@ public class Main {
         int[] ints = new int[]{12,412,4,12,55,12,5,32,123,56,112,53,12};
         HeapSort.heapsort(ints);
         System.out.println(Arrays.toString(ints));
+        System.out.println(romanToArabic("MMXXIV"));
 
+    }
+
+    public static int romanToArabic(String romanNumber) {
+        Map<Character, Integer> romanValues = new HashMap<>();
+        romanValues.put('I', 1);
+        romanValues.put('V', 5);
+        romanValues.put('X', 10);
+        romanValues.put('L', 50);
+        romanValues.put('C', 100);
+        romanValues.put('D', 500);
+        romanValues.put('M', 1000);
+
+        int arabicNumber = 0;
+        int prevValue = 0;
+
+        for (int i = romanNumber.length() - 1; i >= 0; i--) {
+            int currentValue = romanValues.get(romanNumber.charAt(i));
+            if (currentValue < prevValue) {
+                arabicNumber -= currentValue;
+            } else {
+                arabicNumber += currentValue;
+            }
+            prevValue = currentValue;
+        }
+        return arabicNumber;
     }
 }
