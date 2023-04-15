377b97a2fe5ec8b9894a9b5383dd7b11c3090e36
ZiganshinIB <ZiganshinIB@nknh.sibur.ru>
2023-04-15 13:53:50 +0300

    Feat Seminar 5: homework ->HeapSort test

--- src/lesson5/homework/Main.java
+++ src/lesson5/homework/Main.java
@@ -1,6 +1,7 @@
 package lesson5.homework;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -14,7 +15,9 @@ public class Main {
         staffList.add(new Staff("Анна", "Крутова"));
         StaffMap staffMap = new StaffMap(staffList);
         staffMap.print();
-
+        int[] ints = new int[]{12,412,4,12,55,12,5,32,123,56,112,53,12};
+        HeapSort.heapsort(ints);
+        System.out.println(Arrays.toString(ints));
 
     }
 }
